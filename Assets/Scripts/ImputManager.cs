742ee32462b9aefb3364ccfd196d13fe9607740e
PM1hail <158460530+PM1hail@users.noreply.github.com>
2024-09-10 19:44:49 +0300

    initial commit

---
+++ Assets/Scripts/ImputManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ImputManager : MonoBehaviour
+{
+    private PlayerInputs playerInput;
+    private PlayerInputs.WalkActions walk;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInputs();
+        walk = playerInput.Walk;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+        walk.Jump.performed += ctx => motor.Jump();
+
+        walk.Crouch.performed += ctx => motor.Crouch();
+        walk.Sprint.performed += ctx => motor.Sprint();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        motor.ProcessMove(walk.Movement.ReadValue<Vector2>());
+    }
+    void LateUpdate(){
+        look.ProcessLook(walk.Look.ReadValue<Vector2>());
+    }
+    private void OnEnable(){
+        walk.Enable();
+    }
+    private void OnDisable(){
+        walk.Disable();
+    }
+}
